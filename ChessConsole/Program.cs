68eaf2252b7771c8dab50781ed326c438ea68221
Jose Vitor Quadrado <josevitorquadrado@gmail.com>
2022-09-07 13:41:03 -0300

    Creating ChessPosition class

--- ChessConsole/Program.cs
+++ ChessConsole/Program.cs
@@ -10,20 +10,9 @@ namespace ChessConsole
     {
         static void Main(string[] args)
         {
-            try 
-            {
-                Board board = new Board(8, 8);
-
-                board.InsertPiece(new Rook(board, Color.Black), new Position(0, 0));
-                board.InsertPiece(new Rook(board, Color.Black), new Position(1, 3));
-                board.InsertPiece(new King(board, Color.Black), new Position(2, 4));
-
-                Screen.PrintBoard(board);
-            }
-            catch (BoardException error)
-            {
-                Console.WriteLine(error.Message);
-            }
+            ChessPosition position = new ChessPosition('c', 7);
+            Console.WriteLine(position);
+            Console.WriteLine(position.ToPosition());
         }
     }
 }
